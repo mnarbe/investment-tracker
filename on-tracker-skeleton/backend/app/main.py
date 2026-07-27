a7a7510fdb668351ec08e27a989bfe6a79fee170
mnarbe <134221357+mnarbe@users.noreply.github.com>
2026-07-27 15:40:05 -0300

    first frontend, shows ONs, no functionality yet, changed main.py to avoid CORS

--- on-tracker-skeleton/backend/app/main.py
+++ on-tracker-skeleton/backend/app/main.py
@@ -1,16 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import crear_tablas
 from app.routers import bonos
 
 app = FastAPI(title="ON Tracker")
 
+# Para evitar error CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(bonos.router)
+
 
 @app.on_event("startup")
 def on_startup():
-    # Se ejecuta una vez solo cuando arranca el servidor
-    # si todavía no existen (no borra nada si ya existían)
+    # Se ejecuta una sola vez, cuando arranca el servidor: crea las tablas
+    # si todavía no existen (no borra nada si ya existían).
     crear_tablas()
 
 
